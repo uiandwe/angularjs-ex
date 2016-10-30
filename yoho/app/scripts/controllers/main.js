5422510dd52bec1f80f56294bf6f62c8a2bcc835
uiandwe <uiandwe@gmail.com>
2016-10-30 18:01:40 +0900

    yo init

---
+++ yoho/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yohoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yohoApp
+ */
+angular.module('yohoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
