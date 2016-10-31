917394c8d6932013e4fb354e5fe2c5cfa78dcb77
uiandwe <uiandwe@gmail.com>
2016-10-31 21:37:17 +0900

    get test

---
+++ angcms/public/js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+])
+.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ angcms/public/js/controllers.js
@@ -0,0 +1,50 @@
+'use strict';
+angular.module('myApp.controllers', []).
+controller('MovieListCtrl', ['$scope', 'rtmFactory',
+    function($scope, rtmFactory) {
+        $scope.countries = rtmFactory.getCountries();
+
+        $scope.loadMovies = function(countryCode) {
+            rtmFactory.getMovies(countryCode).then(
+                function(response) {
+
+                    var movieArray = response.data.movies;
+                    console.log(JSON.stringify(movieArray))
+                    $scope.movies= movieArray;
+
+
+                },
+                function(response) {
+                    // error message
+                }
+            )
+        };
+
+        $scope.loadMovies('us');
+
+
+    }
+]);
+
+angular.module('myApp.controllers', [])
+  .controller('MainCtrl', function ($scope, $http) {
+
+        $http.get('http://127.0.0.1:3000/api/pages').
+            success(function(data){
+                console.log(data);
+            }
+
+        )
+
+
+var baseURL='http://lorempixel.com/960/450/';
+    $scope.setInterval=5000;
+
+ $scope.slides = [
+ {title:'7 ways to stay fit', image:baseURL+'sports/'},
+ {title:'Healthy Food', image:baseURL+'food/'},
+ {title:'Relaxing Holidays', image:baseURL+'nature/'}
+
+
+ ];
+});
