d8e9853d56aeb5ee008a96fc72f92ba4a71b7c8f
uiandwe <uiandwe@gmail.com>
2016-10-31 20:17:14 +0900

    add page

--- angcms/routes/api.js
+++ angcms/routes/api.js
@@ -20,4 +20,24 @@ router.get('/pages', function(req, res){
     })
 });
 
+
+router.post('/pages/add', function(req, res){
+    var page = new Page({
+        title: req.body.title,
+        url: req.body.url,
+        content: req.body.content,
+        menuIndex: req.body.menuIndex,
+        date: new Date(Date.now())
+    });
+
+    page.save(function(err){
+        if(!err){
+            return res.send(200, page);
+        }
+        else{
+            return res.send(500, err);
+        }
+    });
+});
+
 module.exports = router;
